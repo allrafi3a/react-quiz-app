be1223287c6557fa1d89d991bdf23b1ed09dfe1a
Ahmed Bappy <bappy3a@gmail.com>
2024-02-14 18:29:14 +0600

    Result Page Convat done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import Layout from "./Layout";
 // import Login from "./pages/Login";
 // import Home from "./pages/Home";
 // import Signup from "./pages/Signup";
-import Quiz from "./pages/Quiz";
+// import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <Login /> */}
       {/* <Signup /> */}
       {/* <Home /> */}
-      <Quiz />
+      {/* <Quiz /> */}
+      <Result />
     </Layout>
   );
 }
