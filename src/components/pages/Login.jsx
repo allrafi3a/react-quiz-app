4012e1c06b447c79a0fe73138a6335222280656b
Ahemd Bappy <bappy3a@gmail.com>
2024-02-18 23:49:44 +0600

    login done

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import clesses from '../../styles/Login.module.css'
-import Button from '../Button'
-import Form from '../Form'
-import Illustration from '../Illustration'
-import Textinput from '../Textinput'
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from '../../contexts/AuthContext';
+import clesses from '../../styles/Login.module.css';
+import Button from '../Button';
+import Form from '../Form';
+import Illustration from '../Illustration';
+import Textinput from '../Textinput';
 
 export default function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState();
+
+    const history = useNavigate();
+    const { login } = useAuth();
+
+    async function hendleSumit(e) {
+        e.preventDefault();
+        try {
+            setError("");
+            setLoading(true);
+            await login(email, password);
+            history('/');
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+            setError("Failed to create account");
+        }
+    }
+
     return (
         <>
             <h1>Login to your account</h1>
             <div className="column">
                 <Illustration />
-                <Form className={`${clesses.login}`}>
-                    <Textinput type="text" placeholder="Enter email" icon="alternate_email" />
-                    <Textinput type="password" placeholder="Enter password" icon="lock" />
+                <Form className={`${clesses.login}`} onSubmit={hendleSumit}>
+                    <Textinput type="email" placeholder="Enter email" icon="alternate_email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <Textinput type="password" placeholder="Enter password" icon="lock" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-                    <Button>
+                    <Button disabled={loading} type="submit ">
                         <span>Submit now</span>
                     </Button>
-
-                    <div className="info">Don't have an account? <a href="signup.html">Signup</a> instead.</div>
+                    {error && <p className='error'>{error}</p>}
+                    <div className="info">Don't have an account? <Link to="/signup">Signup</Link> instead.</div>
                 </Form>
             </div>
         </>
